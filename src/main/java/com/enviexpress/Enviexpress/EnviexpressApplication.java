c13872bfa41f9b62572c6166ec8d93667097def9
Juanito <polarmagic2017@gmail.com>
2025-11-18 19:05:31 -0500

    Creacion del repositorio 18/11

---
+++ src/main/java/com/enviexpress/Enviexpress/EnviexpressApplication.java
@@ -0,0 +1,13 @@
+package com.enviexpress.Enviexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnviexpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnviexpressApplication.class, args);
+	}
+
+}
